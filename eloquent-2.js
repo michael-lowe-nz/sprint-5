4c0a68bfa070392820776e6615be6be62d27cf5f
Michael Lowe <mikkeyfluttie@gmail.com>
2016-09-14 11:03:49 +1200

    Add exerise 1

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,5 +1,42 @@
+// Exercise 1
+// Looping a Triangle
 var output = ""
 for(var i = 0; i < 7; i++){
   output += "#";
   console.log(output);
+}
+
+// Exercise 2
+// FizzBuzz
+for (var i = 1; i <= 100; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  }
+  else if(i % 3 === 0){
+    console.log("Fizz");
+  }
+  else if(i % 5 === 0){
+    console.log("Buzz");
+  }
+  else {
+    console.log(i);
+  }
+}
+
+
+//Exercise 3
+//ChessBoard
+var board = "# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n# # # # # # # #\n";
+console.log("Board1"+board);
+
+var output = ""
+var size = 8;
+for (var i = 0; i < size; i++){
+  for (var j = 0; i < size; i++){
+    output += "#"
+    if(j !== size-1){
+      output +=" ";
+    }
+  }
+  output+"\n";
 }
